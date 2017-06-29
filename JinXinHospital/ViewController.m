4c8364a55bbac4013de7a023909adf56cb5e6196
孙苏 <sunsu2010@163.com>
2017-06-29 14:13:42 +0800

    JinXinHospital
     医院项目

---
+++ JinXinHospital/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JinXinHospital
+//
+//  Created by 孙苏 on 2017/6/26.
+//  Copyright © 2017年 sunsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '9.0'
+target 'JinXinHospital' do
+ pod 'AFNetworking','~> 3.1.0'
+ pod 'MJExtension'
+ pod 'WebViewJavascriptBridge'
+ pod 'AliyunOSSiOS'
+ pod 'MJRefresh'
+ 
+
+
+
+
+
+
+end
